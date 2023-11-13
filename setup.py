de2e16595a491dd46c9e8e0a182c0882f4ddb743
jkavya0 <150675175+jkavya0@users.noreply.github.com>
2023-11-13 13:28:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz_game',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your project may have
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz:math_quiz',
+        ],
+    },
+    test_suite='tests',
+)
