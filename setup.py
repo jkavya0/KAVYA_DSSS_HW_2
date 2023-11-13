0271172a697f79b464f054d989d431f017fb3179
jkavya0 <150675175+jkavya0@users.noreply.github.com>
2023-11-13 13:30:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List any dependencies your project may have
-        'numpy',
+
     ],
     entry_points={
         'console_scripts': [
