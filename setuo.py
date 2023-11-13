c3c594b2a1d48be534335990b0d193917107c449
jkavya0 <150675175+jkavya0@users.noreply.github.com>
2023-11-13 13:25:49 +0100

    Update setuo.py

--- setuo.py
+++ setuo.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='math_quiz_game',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your project may have
+        'numpy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz:math_quiz',
+        ],
+    },
+    tests_require=[
+        'unittest',
+    ],
+    test_suite='tests',
+)
